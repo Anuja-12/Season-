f815397a4d910b5bd69cbe0ffca27c5c073cf700
Anuja <aryalanuja99@gmail.com>
2020-08-16 23:07:42 +0545

    upto states

--- src/index.js
+++ src/index.js
@@ -4,16 +4,33 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={lat:null};
+        this.state={lat:null, errorMessage: ' '};
         window.navigator.geolocation.getCurrentPosition(
-            position=> console.log(position),
-            err=> console.log(err));
+            position=> {
+                this.setState({lat: position.coords.latitude});
+            },
+            err=> {
+                this.setState({errorMessage: err.message});
+            }
+            );
      }
     render(){
-    
-
-    return <div>Latitude:{this.state.lat}</div>
+    return <div>
+        Latitude:{this.state.lat}<br/>
+        Error: {this.state.errorMessage }
+        </div>;
     }
-};
 
+
+  /*  render(){
+     if(this.state.errorMessage && !this.state.lat){
+       return <div>Error: {this.state.errorMessage}</div>;
+     }
+     if(this.state.lat && !this.state.errorMessage){
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+      return <div>Loading!</div>;
+    }
+
+*/};
 ReactDOM.render(<App/>, document.querySelector('#root'));
