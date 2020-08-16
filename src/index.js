770b86a275263f82ee0d3c14dd2f5a7fbc058d27
Anuja <aryalanuja99@gmail.com>
2020-08-16 22:06:31 +0545

    third commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
  
-const App=() => {
-    window.navigator.geolocation.getCurrentPosition(
-        position=> console.log(position),
-        err=> console.log(err));
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={lat:null};
+        window.navigator.geolocation.getCurrentPosition(
+            position=> console.log(position),
+            err=> console.log(err));
+     }
+    render(){
+    
 
-  return <div>Latitude:</div>
+    return <div>Latitude:{this.state.lat}</div>
+    }
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
