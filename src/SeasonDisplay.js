abc677b6776e6e6de163d690a6a13ddd5b792d1d
Anuja <aryalanuja99@gmail.com>
2020-08-18 09:38:59 +0545

    6th commit

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+const seasonConfig={
+  summer: {
+    text:'lets hit the beach!',
+    iconName: 'sun'
+  },
+  winter :{
+    text: 'Burr it is cold',
+    iconName: 'snow'
+  }
+};
 
-const SeasonDisplay=()=>{
-    return <div>Season Display</div>;
+const getSeason=(lat, month)=>{
+  if(month>2 && month<9){
+      return lat>0  ? 'summer' : 'winter' ;
+  } 
+  else {
+      return lat >0 ? 'winter' :'summer';
+  }
+
+};
+
+const SeasonDisplay=(props)=>{
+    const season= getSeason(props.lat , new Date().getMonth());
+    const {text, iconName}= seasonConfig[season]
+    return (
+         <div>
+            <i className={`massive ${iconName} icon`}/>
+            <h1> {text}</h1>
+            <i className={`massive  ${iconName} icon`}/>
+          </div>
+);
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
  
 class App extends React.Component{
+    //constructor del
      state={lat:null, errorMessage: ''};
-     
+
      componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             position=> this.setState({lat: position.coords.latitude}),
             err=> this.setState({errorMessage: err.message})
             );
           }
-     componentDidUpdate(){
+     /*componentDidUpdate(){
         console.log('My component was just updated- it rerendered.');
-     }   
+     } */  
     render(){
     return <div>
-        Latitude:{this.state.lat}<br/>
+        <SeasonDisplay  lat ={this.state.lat}/><br/>
         Error: {this.state.errorMessage }
         </div>;
     }
