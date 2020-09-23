29297f04525da4cf8ca40bea36b062966e328e98
Anuja <aryalanuja99@gmail.com>
2020-09-23 21:44:41 +0545

    final one

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
- 
-class App extends React.Component{
-    //constructor del
-     state={lat:null, errorMessage: ''};
+class App extends React.Component {
+  state = { lat: null, errorMessage: '' };
 
-     componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            position=> this.setState({lat: position.coords.latitude}),
-            err=> this.setState({errorMessage: err.message})
-            );
-          }
-     /*componentDidUpdate(){
-        console.log('My component was just updated- it rerendered.');
-     } */  
-    render(){
-    return <div>
-        <SeasonDisplay  lat ={this.state.lat}/><br/>
-        Error: {this.state.errorMessage }
-        </div>;
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
-
-  /*  render(){
-     if(this.state.errorMessage && !this.state.lat){
-       return <div>Error: {this.state.errorMessage}</div>;
-     }
-     if(this.state.lat && !this.state.errorMessage){
-        return <div>Latitude: {this.state.lat}</div>;
-    }
-      return <div>Loading!</div>;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-*/};
-ReactDOM.render(<App/>, document.querySelector('#root'));
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
